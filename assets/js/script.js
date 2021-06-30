2351249795033fc955879d2a435459621a53a29d
professora-francisca-maria <86264326+professora-francisca-maria@users.noreply.github.com>
2021-06-30 17:53:04 -0300

    Versão otimizada

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,37 +26,24 @@ function verificacao() {
 var agora = new Date();
 var hora = agora.getHours();
 var saudacoes = window.document.querySelector('span#saudacoes');
-//var background = window.document.getElementById('background');
 
 
 if (hora <= 5) {
-    //var saudacoes = window.document.querySelector('span#saudacoes');
-    //saudacoes.innerText = 'Boa Madruga!';
     document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
+    saudacoes.innerText = 'Boa Madruga!';
 } else {
     if (hora < 12) {
-        //var saudacoes = window.document.querySelector('span#saudacoes');
-        //saudacoes.innerText = 'Bom dia!';
+        saudacoes.innerText = 'Bom dia!';
     } else {
         if (hora < 18) {
-            //var saudacoes = window.document.querySelector('span#saudacoes');
-
-            document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
             saudacoes.innerText = 'Boa Tarde!';
-
-            //document.getElementById("background").style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
-
-            //background.style.backgroundImage = 'url(../img/Fundo-preto-claro-alterada.png)';
-            
-            //document.body.style.backgroundAttachment = "fixed";
-            //document.body.style.backgroundSize = "30%";
         } else {
-            //var saudacoes = window.document.querySelector('span#saudacoes');
-            //saudacoes.innerText = 'Boa Noite!';
             document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
+            saudacoes.innerText = 'Boa Noite!';
         }
     }
 }
+
 
 //LER MAIS
 
@@ -76,18 +63,4 @@ function lerMais() {
         apareceDesaparece.style.display = "none";
         lerMenos.style.display = 'none';
     }
-
-/*
-    var texto = window.document.getElementById('lerMais');
-
-    texto.innerHTML = `Na qual os mesmo possuem de suas versão física e estão upando para essa plataforma com objetivo de oferecer exclusivamente para uso parcial em pesquisas e estudos acadêmicos. Venda ou distribuição do conteúdo é ilegal. <a href=""><span id='lerMenos'>Ler menos.</span></a>`
-
-    texto.style.color = 'black';
-    texto.style.cursor = 'inherit';
-    texto.style.border = 'none';
-    
-    lerMenos.style.color = 'blue';
-    lerMenos.style.cursor = 'pointer';
-    lerMenos.style.borderBottom = 'blue solid 1px';
-*/
 }
