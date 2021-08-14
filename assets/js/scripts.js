3bacc6af6e9e8cb5e3933a53d0821ce37ff4ffa8
professora-francisca-maria <86264326+professora-francisca-maria@users.noreply.github.com>
2021-08-14 17:10:46 -0300

    Removido código desnecessário

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,9 +20,6 @@ function verificar() {
 
 //SAUDAÇÕES
 
-//let agora = new Date();
-//let milisegundos = 3600000 - (agora.getMinutes() * 60000);
-
 var saudacoes = window.document.querySelector('span#saudacoes');
 
 
@@ -33,8 +30,7 @@ function temporizador() {
     let tempo = new Date();
 
     if (tempo.getHours() <= 5) {
-        //document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
-        verificarAtivacao.checked;//Ativição do Dark-Mode
+        document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
         saudacoes.innerText = 'Boa Madruga!';
     } else {
         if (tempo.getHours() < 12) {
@@ -43,24 +39,12 @@ function temporizador() {
             if (tempo.getHours() < 18) {
                 saudacoes.innerText = 'Boa Tarde!';
             } else {
-                //document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
-                verificarAtivacao.checked;//Ativição do Dark-Mode
+                document.body.style.backgroundImage = "url('assets/img/Fundo-preto-claro-alterada.png')";
                 saudacoes.innerText = 'Boa Noite!';
             }
         }
     }
-
-    //setTimeout(retornarFuncao, milisegundos);
 }
-/*
-function retornarFuncao() {
-    
-    milisegundos = 3600000;
-    return temporizador();
-
-}
-*/
-
 
 
 //LER MAIS
